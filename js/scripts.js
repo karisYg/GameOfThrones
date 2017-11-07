2cde9c20d1dc3cf47283dadac0bb509a57d0a1a1
karisyg <kariukipeter30@gmail.com>
2017-11-07 08:32:00 +0300

    files rewrite

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,44 @@
-
-
-var spanoutput= document.getElementById("output");
-
 $(document).ready(function() {
-	var playerone =document.getElementById("inputplayerone").value;
-	var playertwo =document.getElementById("inputplayertwo").value;
 
+	console.log(localStorage.getItem("playerone"));
+	// var playeronename = prompt("Enter Player One name");
+	// $("#userone").text(playeronename);
+	// var playertwoname= prompt("Enter Player Two name");
+	// $("#usertwo").text(playertwoname);
+
+	$("#inputplayerone").val("");
+	$("#inputplayertwo").val("");
 	$("#inputplayerone").click(function(){
 		$(this).val("");
+		localStorage.removeItem("playerone");
 	});
-
 	$("#inputplayertwo").click(function(){
 		$(this).val("");
+		localStorage.removeItem("playerone");
 
 	});
-
-
-	$("span.playerone").click(function () {
-		alert(playertwo);
-		$("#userone").text("playerone"+playerone);
-
-
+	var playerone =$("#inputplayerone");	
+	$("#spanplayerone").click(function () {
+		var playerone =$("#inputplayerone");
+		playerone=playerone.val();
+		localStorage.setItem("playerone", playerone);		
+		$("#userone").text(playerone);
+	});
+	$("#rollplayerone").click(function(){
+		var gamerone = localStorage.getItem("playerone");
+		if (gamerone===null) {
+			alert("player one is required");
+			
+		}else{
+			var randone = Math.floor((Math.random()*6)+1);
+			$("#rolloneresult").text(randone);
+			//alert(randone);
+		}
 	});
 
-
+	$("#spanplayertwo").click(function(){
+		var playertwo =$("#inputplayertwo").val();
+		$("#usertwo").text(playertwo);
+	});
 	
 });
